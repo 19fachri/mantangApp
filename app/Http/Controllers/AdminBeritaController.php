ed70ef07101e0b33ec8fc8264f5afb8f4ffe8ab9
a <a@g.com>
2016-10-18 09:54:00 +0700

    CRUD Berita Admin

---
+++ app/Http/Controllers/AdminBeritaController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Berita;
+use File;
+
+class AdminBeritaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Berita::orderBy('id','DESC')->paginate(10);
+        return view('admin.berita.index')->with('data', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.berita.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(), array(
+            'judul' => 'required',
+            'deskripsi' => 'required',
+            'cover' => 'required',
+            ));
+        if($validation->fails()){
+            return redirect()->route('berita.create');
+        }else{
+            $logo = $request->file('cover');
+            $upload = 'uploads/cover/';
+            $filename = str_random(9);
+            $success = $logo->move($upload, $filename);
+
+            if($success){
+            Berita::create([
+                'judul' => $request->input('judul'),
+                'deskripsi' => $request->input('deskripsi'),
+                'cover' => $filename,
+                ]);
+            return redirect()->route('berita.index');
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = Berita::find($id);
+        return view('admin.berita.edit')->with('data', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = Berita::find($id);
+        $filename = $data->cover;
+        if (!empty($request->cover)) {
+            File::delete('uploads/cover'.$data->cover);
+            $logo = $request->file('cover');
+            $upload = 'uploads/cover/';
+            $filename = $data->cover;
+            $success = $logo->move($upload, $filename);    
+        }
+        Berita::find($id)->update(['judul' => $request->judul, 'cover' => $filename, 'deskripsi' => $request->deskripsi]);
+        return redirect()->route('berita.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = Berita::find($id);
+        File::delete('uploads/berita/'.$data->gambar);
+        Berita::destroy($id);
+        return redirect()->route('berita.index');
+    }
+}
---
+++ resources/views/admin/berita/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('head')
+	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+	<script>tinymce.init({ selector:'textarea' });</script>
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+    	<form class="form-horizontal" role="form" method="POST" action="<% url('/admin/berita') %>" enctype="multipart/form-data">
+        <% csrf_field() %>
+
+	        <div class="col-md-8">
+	            <div class="form-group<% $errors->has('judul') ? ' has-error' : '' %>">
+	                <div class="col-md-12">
+	                    <input id="judul" type="text" class="form-control" name="judul" value="<% old('judul') %>" placeholder="Judul Berita" required autofocus>
+
+	                    @if ($errors->has('judul'))
+	                        <span class="help-block">
+	                            <strong><% $errors->first('judul') %></strong>
+	                        </span>
+	                    @endif
+	                </div>
+	            </div>
+
+	            <div class="form-group<% $errors->has('deskripsi') ? ' has-error' : '' %>">
+	                <div class="col-md-12">
+	                    <textarea id="deskripsi" name="deskripsi"></textarea>
+	                </div>
+	            </div>
+
+	            <div class="form-group">
+	                <div class="col-md-6 col-md-offset-4">
+	                    <button type="submit" class="btn btn-primary">
+	                        Simpan
+	                    </button>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-md-4">
+	        	<div class="form-group<% $errors->has('cover') ? ' has-error' : '' %>">
+	                <div class="col-md-12">
+	                    <input id="cover" type="file" class="form-control" name="cover" placeholder="cover" required autofocus>
+	                </div>
+	            </div>
+	        </div>
+	    
+	    </form>
+	</div>
+</div>
+@endsection
