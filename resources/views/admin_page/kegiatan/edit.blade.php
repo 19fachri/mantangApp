62db680c40b9ddd007c4c8cba40ba33b45ae7372
diazsasak <diazfebrian2@gmail.com>
2016-10-25 08:02:34 +0800

    komentar beres. tinggal fitur suara rakyat

--- resources/views/admin_page/kegiatan/edit.blade.php
+++ resources/views/admin_page/kegiatan/edit.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.admin_layout')
-
+@section('title')
+Edit Kegiatan
+@endsection
 @section('head')
 	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 	<script>tinymce.init({ selector:'textarea' });</script>
--- resources/views/public_page/beritaDetail.blade.php
+++ resources/views/public_page/beritaDetail.blade.php
@@ -29,17 +29,18 @@
 </div>
 @endsection
 @section('script')
-<!-- <script>
+<script>
 
 /**
 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
+
 var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+this.page.url = '<% url()->full() %>';  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = '<% url()->full() %>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+
 };
-*/
+
 (function() { // DON'T EDIT BELOW THIS LINE
 	var d = document, s = d.createElement('script');
 	s.src = '//desamantang.disqus.com/embed.js';
@@ -48,4 +49,4 @@ this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your pag
 })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
- -->@endsection
+@endsection
